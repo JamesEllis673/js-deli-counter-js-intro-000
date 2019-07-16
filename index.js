d3862771987ef56db08407bc9a5d41627ca0b1ea
James Ellis <13jellis@toothillschool.co.uk>
2019-07-16 10:57:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ function takeANumber (line, name) {
   line.push (name)
   return ("Welcome, " + name + ". You are number " + line.length + " in line.")
 }
+
+function nowServing (line) {
+  if line.length === 0 {
+    return ("Nobody is waiting to be served!")
+  }
+  else {
+    var name = line[0]
+    line.shift()
+    return (name)
+  }
+}
+
+function currentLine (line) {
+  if line.length === 0 {
+    return ("The line is currently empty")
+  }
+  else {
+    var stringLine = "The line is currently: "
+    for (var i = 1; i <= line.length; i++) {
+      stringLine = stringLine + i + ". " + line[i-1] + ", "
+    }
+  }
+  return stringLine
+}
