a7f570fe7f11e405db26123aa68cfec4617faf83
James Ellis <13jellis@toothillschool.co.uk>
2019-07-16 11:03:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,23 +5,26 @@ function takeANumber (line, name) {
 
 function nowServing (line) {
   if (line.length === 0) {
-    return ("Nobody is waiting to be served!")
+    return ("There is nobody waiting to be served!")
   }
   else {
     var name = line[0]
     line.shift()
-    return (name)
+    return ("Currently serving " + name + ".")
   }
 }
 
 function currentLine (line) {
   if (line.length === 0) {
-    return ("The line is currently empty")
+    return ("The line is currently empty.")
   }
   else {
     var stringLine = "The line is currently: "
     for (var i = 1; i <= line.length; i++) {
-      stringLine = stringLine + i + ". " + line[i-1] + ", "
+      stringLine = stringLine + i + ". " + line[i-1]
+      if (i != line.length) {
+        stringLine = stringLine + ", "
+      }
     }
   }
   return stringLine
