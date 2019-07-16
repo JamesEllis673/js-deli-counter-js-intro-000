c07df12cebc21ba6f60f03f819f8fd77308c9b0f
James Ellis <13jellis@toothillschool.co.uk>
2019-07-16 10:59:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber (line, name) {
 }
 
 function nowServing (line) {
-  if (line.length) == 0 {
+  if (line.length === 0) {
     return ("Nobody is waiting to be served!")
   }
   else {
@@ -15,7 +15,7 @@ function nowServing (line) {
 }
 
 function currentLine (line) {
-  if (line.length) == 0 {
+  if (line.length === 0) {
     return ("The line is currently empty")
   }
   else {
